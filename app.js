f4271dc7f7e87f9f5e1703f46484a63e73ed855e
jackyan63@gmail.com <jackyan63@gmail.com>
2016-10-25 15:08:42 -0400

    Complete the click function and message function.

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 (function (){
 'use strict';
 
-angular.module('myFirstApp',[])
+angular.module('DIApp',[])
 
-.controller('MyFirstController', function ($scope) {
-  $scope.name = "Yaakov";
+.controller('DIController', function ($scope, $filter) {
+
+
+  $scope.upper = function() {
+  	var upCase = $filter('uppercase');
+  	$scope.name = upCase($scope.name);
+  }
+
+  $scope.checkAmount = function(){
+  	console.log($scope.name.split(" ").length);
+  	if($scope.name.split(" ").length <= 3){
+  		$scope.stateOfBeing = "Enjoy!";
+  	}
+  	if($scope.name.split(" ").length > 3){
+  		$scope.stateOfBeing = "Too Much!";
+  	}
+  	if($scope.name == ""){
+  		$scope.stateOfBeing = "Please enter the data first!";
+  	}
+
+  }
   $scope.sayHello = function() {
     return "Hello Coursera!";
   };
